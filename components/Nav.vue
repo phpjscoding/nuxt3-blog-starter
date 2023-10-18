b6ceff7181c72cd01865a70a15072ef04a6dc55f
phpjscoding <phpjscoder@gmail.com>
2023-10-18 03:55:08 +0100

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+         <ul class="flex bg-gray-400">
+             <li v-for="link in links" :key="link.name" class="p-4 bg-green-400"> <nuxt-link :to="link.link">
+                {{link.name}}
+            </nuxt-link> </li>
+         </ul>
+    </div>
+</template>
+<script setup>
+ const links = [
+    {name:'home',link:'/'},
+    {name:'categories',link:'/categories'},
+    {name : 'blog',link:'/blog'}
+ ]
+
+</script>
+<style lang="">
+    
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <Nav />
+        <slot />
+        <Footer />
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
